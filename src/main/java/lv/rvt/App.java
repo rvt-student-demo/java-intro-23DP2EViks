105c010f24d9506b94aa1e537952f40b5fc3105e
23DP2EViks <166109764+23DP2EViks@users.noreply.github.com>
2024-09-19 10:26:34 +0000

    only positives

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,28 +1,26 @@
 package lv.rvt;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        int number = 101;
+import java.util.Scanner;
 
-        if (number < 0) {
-            System.out.println("Impossible");
-        } else if (number >= 0 && number <= 49) {
-            System.out.println("failed");
-        } else if (number >= 50 && number <= 59) {
-            System.out.println("1");
-        } else if (number >= 60 && number <= 69) {
-            System.out.println("2");
-        } else if (number >= 70 && number <= 79) {
-            System.out.println("3");
-        } else if (number >= 80 && number <= 89) {
-            System.out.println("4");
-        } else if (number >= 90 && number <= 100) {
-            System.out.println("5");
-        } else {
-            System.out.println("incredible!");
-        }
+public class App 
+    {public static void main( String[] args )
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Input a number");
+            int number = Integer.valueOf(scanner.nextLine());
+
+            if (number == 0) {
+        break;
     }
 
+    if (number < 0) {
+        System.out.println("Unsuitable number");
+        continue;
+    }
+
+    System.out.println(number * number);
+    }
+}
 }
