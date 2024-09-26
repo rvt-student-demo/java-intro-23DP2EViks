b0932075a18184fe808a132702213ea8d537255b
23DP2EViks <166109764+23DP2EViks@users.noreply.github.com>
2024-09-26 09:50:18 +0000

    Conversation

--- src/main/java/lv/rvt/Greeting.java
+++ src/main/java/lv/rvt/Greeting.java
@@ -3,10 +3,13 @@ package lv.rvt;
 import java.util.Scanner;
 public class Greeting {
     public static void main(String[] args) {
-        System.out.println("What\'s your name");
         Scanner scanner = new Scanner(System.in);
-        String message = scanner.nextLine();
-        String start = "Hi ";
-        System.out.println(start + message);
+        System.out.println("Greetings! How are you doing?");
+        String first = scanner.nextLine();
+        
+        System.out.println("Oh, how interesting. Tell me more!");
+        String second = scanner.nextLine();
+        
+        System.out.println("Thanks for sharing!");
     }
 }
