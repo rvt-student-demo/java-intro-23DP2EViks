9436fb65d612416d333cb9466dd34cac31a313d6
23DP2EViks <166109764+23DP2EViks@users.noreply.github.com>
2024-09-26 09:25:49 +0000

    Hi Ada Lovelace

---
+++ src/main/java/lv/rvt/HiAdaLovelace.java
@@ -0,0 +1,12 @@
+package lv.rvt;
+
+import java.util.Scanner;
+public class HiAdaLovelace {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String name = " Ada Lovelace";
+        String start = "Hi";
+        String end = "!";
+        System.out.println(start + name + end);
+    }
+}
