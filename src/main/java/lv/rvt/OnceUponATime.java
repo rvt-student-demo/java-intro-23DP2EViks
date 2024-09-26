4d3fdb15f7040aa4f9ef018358581d79caa58abf
23DP2EViks <166109764+23DP2EViks@users.noreply.github.com>
2024-09-26 09:16:51 +0000

    Dinosaur

--- src/main/java/lv/rvt/OnceUponATime.java
+++ src/main/java/lv/rvt/OnceUponATime.java
@@ -4,6 +4,6 @@ public class OnceUponATime {
     public static void main(String[] args) {
         System.out.println("Once upon a time");
         System.out.println("there was");
-        System.out.println("a programm");
+        System.out.println("a dinosaur");
     }
 }
