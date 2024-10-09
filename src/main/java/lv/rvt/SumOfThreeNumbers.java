0979d473712ffec5bb3fe987bc91a7926251f74b
23DP2EViks <166109764+23DP2EViks@users.noreply.github.com>
2024-10-09 10:57:54 +0000

    SumOfThreeNumbers

--- src/main/java/lv/rvt/SumOfThreeNumbers.java
+++ src/main/java/lv/rvt/SumOfThreeNumbers.java
@@ -1,7 +1,7 @@
 package lv.rvt;
 
 import java.util.Scanner;
-public class SumOfTwoNumbers {
+public class SumOfThreeNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -9,7 +9,9 @@ public class SumOfTwoNumbers {
         int first = Integer.valueOf(scanner.nextLine());
         System.out.println("Give the second number:");
         int second = Integer.valueOf(scanner.nextLine());
-        int summa = first + second;
+        System.out.println("Give the third number:");
+        int third = Integer.valueOf(scanner.nextLine());
+        int summa = first + second + third;
         System.out.println("The sum of the numbers is " + summa);
     }
 }
