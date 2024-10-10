38b62205c103cb86f1b77b1bc8f90b3e8edde574
23DP2EViks <166109764+23DP2EViks@users.noreply.github.com>
2024-10-10 08:23:55 +0000

    Positivity

---
+++ src/main/java/lv/rvt/Positivity.java
@@ -0,0 +1,16 @@
+package lv.rvt;
+
+import java.util.Scanner;
+public class Positivity {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Give a number:");
+        int first = Integer.valueOf(scanner.nextLine());
+        if (first > 0) {
+            System.out.println("The number is positive.");
+        } else {
+            System.out.println("The number is not positive.");
+        }
+    }
+}
