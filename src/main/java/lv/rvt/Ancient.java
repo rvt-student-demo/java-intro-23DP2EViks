9730ba0a07301b0ac8a32bbcb0a3cf64489fd9ec
23DP2EViks <166109764+23DP2EViks@users.noreply.github.com>
2024-10-10 08:20:50 +0000

    Ancient

--- src/main/java/lv/rvt/Ancient.java
+++ src/main/java/lv/rvt/Ancient.java
@@ -1,14 +1,14 @@
 package lv.rvt;
 
 import java.util.Scanner;
-public class SpeedingTicket {
+public class Ancient {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Give speed:");
+        System.out.println("Give a year:");
         int first = Integer.valueOf(scanner.nextLine());
-        if (first > 120) {
-            System.out.println("Speed Ticket!");
+        if (first < 2017) {
+            System.out.println("Ancient history!");
         }
     }
 }
