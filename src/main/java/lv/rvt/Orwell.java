0232a28eac0b1e8b41ab8ac3da7c4382e4a47666
23DP2EViks <166109764+23DP2EViks@users.noreply.github.com>
2024-10-10 08:18:12 +0000

    SpeedingTicket

--- src/main/java/lv/rvt/Orwell.java
+++ src/main/java/lv/rvt/Orwell.java
@@ -1,14 +1,14 @@
 package lv.rvt;
 
 import java.util.Scanner;
-public class Orwell {
+public class SpeedingTicket {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Give speed:");
         int first = Integer.valueOf(scanner.nextLine());
-        if (first == 1984) {
-            System.out.println("Orwell");
+        if (first > 120) {
+            System.out.println("Speed Ticket!");
         }
     }
 }
