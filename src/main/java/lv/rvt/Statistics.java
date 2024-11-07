0f16c4dbc7410229bf7b9a4e4f008a3f988468ae
23DP2EViks <166109764+23DP2EViks@users.noreply.github.com>
2024-11-07 08:32:53 +0000

    Statistics_part2

--- src/main/java/lv/rvt/Statistics.java
+++ src/main/java/lv/rvt/Statistics.java
@@ -7,15 +7,25 @@ public class Statistics {
     
         public Statistics() {
             this.count = 0;
+            this.sum = 0;
           
         }
     
         public void addNumber(int number) {
             this.count++;
+            this.sum += number;
             
         }
     
         public int getCount() {
             return this.count;
+        }
+        public int sum() {
+            return this.sum;
+        }
+    
+        public double average() {
+            
+            return(double) this.sum / this.count;
         }
     }
