be3070de04c16c07ce190af28f483be90bc01db7
23DP2EViks <166109764+23DP2EViks@users.noreply.github.com>
2024-11-14 08:31:20 +0000

    Payment_card_1

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,31 +1,16 @@
 package lv.rvt;
-import java.util.Scanner;
 public class App {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Statistics allNumbers = new Statistics();
-        Statistics evenNumber = new Statistics();
-        Statistics oddNumber = new Statistics();
-        
-        
-        System.out.println("Enter numbers");
-        
-        while (true){
-        int number = scanner.nextInt();
-        if (number == -1) {
-            break;
+    public class Main {
+        public static void main(String[] args) {
+            PaymentCard card = new PaymentCard(50);
+            System.out.println(card);
+    
+            card.eatAffordably();
+            System.out.println(card);
+    
+            card.eatHeartily();
+            card.eatAffordably();
+            System.out.println(card);
         }
-        allNumbers.addNumber(number);
-        if (number % 2 == 0) {
-            evenNumber.addNumber(number);
-        } else {
-            oddNumber.addNumber(number);
-        }
-        
-    }
-    System.out.println("Sum: " + allNumbers.sum());
-    System.out.println("Sum of even numbers: " + evenNumber.sum());
-    System.out.println("Sum of odd numbers: " + oddNumber.sum());
-    scanner.close();
     }
 }
